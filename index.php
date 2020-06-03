33ae68f2392415a1e829f1412f4decd884d2ebc2
moonlightboss <63856802+moonlightboss@users.noreply.github.com>
2020-06-03 15:28:30 +0600

    Add files via upload
    zadacha 2

---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname = "zadacha2_php";
+
+try{
+	$conn = new PDO ("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+	$sql= "CREATE TABLE `tablica1` (
+  		`id` INT(11) NOT NULL,
+  		`date` timestamp NOT NULL,
+  		`text` text,
+  		PRIMARY KEY (`id`)
+		);";
+
+	$conn->exec($sql);
+    echo "Таблица создана ";
+    $sql= "INSERT INTO `tablica1` (`id`, `date`, `text`) VALUES ('1', '2020-06-03', 'Хорошая погода')";
+    $conn->exec($sql);
+    echo " Запись 1 добавлена";
+
+    $sql= "INSERT INTO `tablica1` (`id`, `date`, `text`) VALUES ('2', '2000-02-01', 'создал табличку')";
+    $conn->exec($sql);
+    echo " Запись 2 добавлена";
+
+    $sql= "INSERT INTO `tablica1` (`id`, `date`, `text`) VALUES ('3', '1999-01-23', 'и заполнил')";
+    $conn->exec($sql);
+    echo " Запись 3 добавлена";
+	
+}
+catch(PDOException $e){
+	echo $sql . $e->getMessage();
+}
+$conn= null;
+
+?>
